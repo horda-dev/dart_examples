bd8938e8605f19003cc52c008ebd51bdb3d83018
Alex Rudyk <alex@horda.dev>
2025-08-28 16:26:42 -0700

    add frozen view

--- counter/server/lib/src/entities.dart
+++ counter/server/lib/src/entities.dart
@@ -129,27 +129,44 @@ class CounterState extends EntityState {
 class CounterViewGroup extends EntityViewGroup {
   final ValueView<String> nameView;
   final CounterView valueView;
+  final ValueView<String> frozenStateView;
 
   CounterViewGroup.fromInitEvent(CounterCreatedEvent event)
     : nameView = ValueView(name: 'name', value: event.name),
-      valueView = CounterView(name: 'value', value: event.count);
+      valueView = CounterView(name: 'value', value: event.count),
+      frozenStateView = ValueView(name: 'isFroze', value: "not frozen");
 
   void incremented(CounterIncrementedEvent event) {
     valueView.increment(event.amount);
+  }
+
+  void decremented(CounterDecrementedEvent event) {
+    valueView.decrement(event.amount);
+  }
+
+  void freezeChanged(CounterFreezeChangedEvent event) {
+    if (event.newValue) {
+      frozenStateView.value = "frozen";
+    } else {
+      frozenStateView.value = "not frozen";
+    }
   }
 
   @override
   void initViews(ViewGroup views) {
     views
       ..add(nameView)
-      ..add(valueView);
+      ..add(valueView)
+      ..add(frozenStateView);
   }
 
   @override
   void initProjectors(EntityViewGroupProjectors projectors) {
     projectors
       ..addInit<CounterCreatedEvent>(CounterViewGroup.fromInitEvent)
-      ..add<CounterIncrementedEvent>(incremented);
+      ..add<CounterIncrementedEvent>(incremented)
+      ..add<CounterDecrementedEvent>(decremented)
+      ..add<CounterFreezeChangedEvent>(freezeChanged);
   }
 }
 
