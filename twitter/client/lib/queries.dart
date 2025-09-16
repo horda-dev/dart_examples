39f18428053201dcf92133fb65d34a8cd2c2f30f
viacheslav4734646 <viacheslav@horda.dev>
2025-09-17 01:28:50 +0300

    fix query recursion, refactor queries

--- twitter/client/lib/queries.dart
+++ twitter/client/lib/queries.dart
@@ -1,17 +1,30 @@
 import 'package:horda_client/horda_client.dart';
 
-// Query for a single Tweet
 class TweetQuery extends EntityQuery {
-  final authorUser = EntityRefView<UserAccountQuery>(
+  final authorUser = EntityRefView(
     'authorUserView',
-    query: UserAccountQuery(),
+    query: BasicUserInfoQuery(),
   );
-  final text = EntityValueView<String>('textView');
-  final createdAt = EntityDateTimeView('createdAtView', isUtc: true);
-  final likeCount = EntityCounterView('likeCountView');
-  final retweetCount = EntityCounterView('retweetCountView');
-  final comments = EntityListView<CommentQuery>(
-    'commentsView', // Assuming TweetViewGroup has a commentsView
+
+  final text = EntityValueView<String>(
+    'textView',
+  );
+
+  final createdAt = EntityDateTimeView(
+    'createdAtView',
+    isUtc: true,
+  );
+
+  final likeCount = EntityCounterView(
+    'likeCountView',
+  );
+
+  final retweetCount = EntityCounterView(
+    'retweetCountView',
+  );
+
+  final comments = EntityListView(
+    'commentsView',
     query: CommentQuery(),
   );
 
@@ -27,74 +40,26 @@ class TweetQuery extends EntityQuery {
   }
 }
 
-// Query for a User Profile
-class UserProfileQuery extends EntityQuery {
-  final displayName = EntityValueView<String>('displayNameView');
-  final bio = EntityValueView<String>('bioView');
-
-  @override
-  void initViews(EntityQueryGroup views) {
-    views
-      ..add(displayName)
-      ..add(bio);
-  }
-}
-
-// Query for a User's Timeline
-class TimelineQuery extends EntityQuery {
-  final tweets = EntityListView<TweetQuery>('tweetsView', query: TweetQuery());
-
-  @override
-  void initViews(EntityQueryGroup views) {
-    views.add(tweets);
-  }
-}
-
-// New UserAccountQuery
-class UserAccountQuery extends EntityQuery {
-  final handle = EntityValueView<String>('handleView');
-  final email = EntityValueView<String>('emailView');
-  final profile = EntityRefView('profileView', query: UserProfileQuery());
-  final followers = EntityListView(
-    'followersView',
-    query: UserAccountQuery(), // Nested query for followers
-  );
-  final following = EntityListView(
-    'followingView',
-    query: UserAccountQuery(), // Nested query for following
-  );
-  final followerCount = EntityCounterView('followerCountView');
-  final followingCount = EntityCounterView('followingCountView');
-  final registeredAt = EntityDateTimeView('registeredAtView', isUtc: true);
-  final timeline = EntityRefView(
-    // New timeline view
-    'timelineView',
-    query: TimelineQuery(),
-  );
-
-  @override
-  void initViews(EntityQueryGroup views) {
-    views
-      ..add(handle)
-      ..add(email)
-      ..add(profile)
-      ..add(followers)
-      ..add(following)
-      ..add(followerCount)
-      ..add(followingCount)
-      ..add(registeredAt)
-      ..add(timeline); // Add timeline view
-  }
-}
-
-// Query for a single Comment
 class CommentQuery extends EntityQuery {
-  final authorUser = EntityRefView('authorUserView', query: UserAccountQuery());
-  final text = EntityValueView<String>('textView');
-  final createdAt = EntityDateTimeView('createdAtView', isUtc: true);
-  final likeCount = EntityCounterView('likeCountView');
+  final authorUser = EntityRefView(
+    'authorUserView',
+    query: BasicUserInfoQuery(),
+  );
+
+  final text = EntityValueView<String>(
+    'textView',
+  );
+
+  final createdAt = EntityDateTimeView(
+    'createdAtView',
+    isUtc: true,
+  );
+
+  final likeCount = EntityCounterView(
+    'likeCountView',
+  );
+
   final replies = EntityListView<CommentQuery>(
-    // Nested replies
     'repliesView',
     query: CommentQuery(),
   );
@@ -110,7 +75,108 @@ class CommentQuery extends EntityQuery {
   }
 }
 
-// Query for Explore Feed
+class UserAccountQuery extends EntityQuery {
+  final handle = EntityValueView<String>(
+    'handleView',
+  );
+
+  final email = EntityValueView<String>(
+    'emailView',
+  );
+
+  final profile = EntityRefView(
+    'profileView',
+    query: UserProfileQuery(),
+  );
+
+  final followers = EntityListView(
+    'followersView',
+    query: BasicUserInfoQuery(),
+  );
+
+  final following = EntityListView(
+    'followingView',
+    query: BasicUserInfoQuery(),
+  );
+
+  final followerCount = EntityCounterView(
+    'followerCountView',
+  );
+
+  final followingCount = EntityCounterView(
+    'followingCountView',
+  );
+
+  final registeredAt = EntityDateTimeView(
+    'registeredAtView',
+    isUtc: true,
+  );
+
+  @override
+  void initViews(EntityQueryGroup views) {
+    views
+      ..add(handle)
+      ..add(email)
+      ..add(profile)
+      ..add(followers)
+      ..add(following)
+      ..add(followerCount)
+      ..add(followingCount)
+      ..add(registeredAt);
+  }
+}
+
+class UserProfileQuery extends EntityQuery {
+  final displayName = EntityValueView<String>('displayNameView');
+  final bio = EntityValueView<String>('bioView');
+
+  @override
+  void initViews(EntityQueryGroup views) {
+    views
+      ..add(displayName)
+      ..add(bio);
+  }
+}
+
+class BasicUserInfoQuery extends EntityQuery {
+  final handle = EntityValueView<String>('handleView');
+  final profile = EntityRefView('profileView', query: DisplayNameQuery());
+
+  @override
+  void initViews(EntityQueryGroup views) {
+    views
+      ..add(handle)
+      ..add(profile);
+  }
+}
+
+class DisplayNameQuery extends EntityQuery {
+  final displayName = EntityValueView<String>('displayNameView');
+
+  @override
+  void initViews(EntityQueryGroup views) {
+    views.add(displayName);
+  }
+}
+
+class UserTimelineQuery extends EntityQuery {
+  final timeline = EntityRefView('timelineView', query: TimelineQuery());
+
+  @override
+  void initViews(EntityQueryGroup views) {
+    views.add(timeline);
+  }
+}
+
+class TimelineQuery extends EntityQuery {
+  final tweets = EntityListView<TweetQuery>('tweetsView', query: TweetQuery());
+
+  @override
+  void initViews(EntityQueryGroup views) {
+    views.add(tweets);
+  }
+}
+
 class ExploreFeedQuery extends EntityQuery {
   final tweets = EntityListView<TweetQuery>('tweetsView', query: TweetQuery());
 
