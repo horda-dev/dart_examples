2e49abdc056c25ab7d09aed45222bc8af0513c30
viacheslav4734646 <viacheslav@horda.dev>
2025-09-18 01:37:56 +0300

    set project id and client api key

--- twitter/client/lib/config.dart
+++ twitter/client/lib/config.dart
@@ -1,4 +1,5 @@
-const kProjectId = 'YOUR_PROJECT_ID';
-const kApiKey = 'YOUR_API_KEY';
+const kProjectId = 'd35fcs4gc98s738ue7b0';
+const kApiKey =
+    'sk-client-94bfc6abc22877d61rDu13dOoiDRXa7DaodimSdzdJ4tjs97zFqN3JDYP1w-_j_ORt9F_5qxdAYuMPo7s25vL40SXJHVht-8FbNxuw==';
 
 final kUrl = 'wss://api.horda.ai/$kProjectId/client';
