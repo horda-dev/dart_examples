a4894859c354c7e3a36077709caa54f5c486d949
viacheslav4734646 <viacheslav@horda.dev>
2025-09-18 19:21:39 +0300

    use a new horda project in the config

--- twitter/client/lib/config.dart
+++ twitter/client/lib/config.dart
@@ -1,5 +1,5 @@
-const kProjectId = 'd360qt4gc98s738ue7bg';
+const kProjectId = 'd3629tsgc98s738ue7c0';
 const kApiKey =
-    'sk-client-13779e4239135d7bux1JG6MM4Rw5OYCAeZ07vrt5DQq5z8m7HaNIOsIf5Vy0UrJmmQmTqV_Fvx4b6Tv_lQngCyPBwiiIxJLht4zBug==';
+    'sk-client-abdc722cabc01423XXZcy3VgHrwO5XHXlGj54T0yc-w68j3oFSJaNUdaln41Ft9YEgu6Dt1CSNJp06_7coyxxA136d5nXVQijpoptQ==';
 
 final kUrl = 'wss://api.horda.ai/$kProjectId/client';
