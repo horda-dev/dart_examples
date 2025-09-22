9d1cad6542fe24755c540cb166958ee13efc2d3f
viacheslav4734646 <viacheslav@horda.dev>
2025-09-22 16:45:15 +0300

    remove unused reopenWebSocket() from auth service

--- twitter/client/lib/auth.dart
+++ twitter/client/lib/auth.dart
@@ -2,19 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:horda_client/horda_client.dart';
 import 'package:logging/logging.dart';
 
-import 'config.dart';
-
 final kAuthService = FirebaseAuthService();
 
 class FirebaseAuthService implements AuthProvider {
   FirebaseAuthService();
 
   final logger = Logger('Auth');
-
-  Future<void> reopenWebSocket({required HordaProcessContext context}) async {
-    final conn = LoggedInConfig(url: kUrl, apiKey: kApiKey);
-    context.changeConnection(conn);
-  }
 
   @override
   Future<String?> getIdToken() async {
