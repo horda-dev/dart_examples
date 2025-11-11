da7325d43fc3c1f227ea946a0d9e9a956ffacd15
viacheslav4734646 <viacheslav@horda.dev>
2025-11-12 00:28:29 +0200

    update client api keys

--- twitter/client/lib/config.dart
+++ twitter/client/lib/config.dart
@@ -1,6 +1,6 @@
 const kProjectId = 'd368c1sgc98s738ue7cg';
 const kApiKey =
-    'sk-client-1Q02wJvUuZkkDMCwlnlGdmdKbJYP8fdQ9p0P5Wq8PLv4XKLlPN-UGeHPU0nsQZqYuDooX6XX9sMyjy1UoSuGYg';
+    'sk-client-J0kPWPBle9SqLJcGtjPWyXyNArS-m-ko7V44IQuVV783FGlWCssXou6BfNxHHq1sXyuIqr2waCKrsYeWwF-e4Q';
 
 final kUrl = 'wss://api.horda.dev/$kProjectId/client';
 // final kUrl = 'ws://localhost:8080/client'; // For local development
