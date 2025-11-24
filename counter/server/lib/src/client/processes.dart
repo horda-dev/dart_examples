5a5d36e1cdfff9d136e0b691b2e32ddc1b496bcc
viacheslav4734646 <viacheslav@horda.dev>
2025-11-24 21:07:43 +0200

    regroup counter business processes

--- counter/server/lib/src/client/processes.dart
+++ counter/server/lib/src/client/processes.dart
@@ -8,12 +8,18 @@ import 'processes/freeze_counter_requested_process.dart';
 import 'processes/increment_counter_requested_process.dart';
 import 'processes/unfreeze_counter_requested_process.dart';
 
-/// Process handler for client-initiated counter operations.
+// Process groups organizig business processes into logical units.
+//
+// A ProcessGroup is a collection of related process functions that respond to
+// client requests. Each process function implements business logic by coordinating
+// between entities, services, and other system components.
+
+/// Process group for counter lifecycle management.
 ///
-/// This class orchestrates counter-related workflows by coordinating between
-/// entities and services. It handles counter creation, deletion, modification,
-/// and list management operations initiated by clients.
-class ClientProcesses extends ProcessGroup {
+/// Handles the creation and deletion of counter entities. These processes
+/// manage the full lifecycle from counter instantiation to removal from
+/// the system.
+class CounterLifecycleProcesses extends ProcessGroup {
   @override
   void registerFuncs(ProcessFuncs funcs) {
     funcs
@@ -24,7 +30,19 @@ class ClientProcesses extends ProcessGroup {
       ..add<DeleteCounterRequested>(
         clientDeleteCounterRequested,
         DeleteCounterRequested.fromJson,
-      )
+      );
+  }
+}
+
+/// Process group for counter operations on existing counters.
+///
+/// Handles value modifications (increment/decrement) and state changes
+/// (freeze/unfreeze) for counters. These processes operate on counters
+/// that have already been created.
+class CounterOperationProcesses extends ProcessGroup {
+  @override
+  void registerFuncs(ProcessFuncs funcs) {
+    funcs
       ..add<IncrementCounterRequested>(
         clientIncrementCounterRequested,
         IncrementCounterRequested.fromJson,
